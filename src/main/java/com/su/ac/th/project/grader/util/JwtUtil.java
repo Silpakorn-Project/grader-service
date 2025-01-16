8bc1cd055c706d70ea81509ab1934bcd00b2833c
sodtiruk <sodtiruk22@gmail.com>
2025-01-16 15:37:01 +0700

    feat: add jwt util

---
+++ src/main/java/com/su/ac/th/project/grader/util/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.su.ac.th.project.grader.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private Key getSigningKey() {
+        String SECRET_KEY = "mySecretKey123456789012345678901234";
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String username) {
+        int EXPIRATION_TIME = 1000 * 60 * 60;
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}

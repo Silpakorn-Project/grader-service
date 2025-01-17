4953c43fc01838532d8d116aa80d909a7912fc3e
sodtiruk <sodtiruk22@gmail.com>
2025-01-17 17:55:41 +0700

    refactor: change format datetime

--- src/main/java/com/su/ac/th/project/grader/util/CommonUtil.java
+++ src/main/java/com/su/ac/th/project/grader/util/CommonUtil.java
@@ -1,11 +1,14 @@
 package com.su.ac.th.project.grader.util;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class CommonUtil {
 
-    public static LocalDateTime getDateTimeNow(){
-        return LocalDateTime.now();
+    public static String getDateTimeNow(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return now.format(formatter);
     }
 
 }

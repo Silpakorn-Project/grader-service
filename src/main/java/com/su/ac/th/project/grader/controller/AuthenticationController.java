a4b320b0caadb71d77a04afcc20d1e97a9a2786a
sodtiruk <sodtiruk22@gmail.com>
2025-01-20 15:23:13 +0700

    refactor: remove unused import and variable

--- src/main/java/com/su/ac/th/project/grader/controller/AuthenticationController.java
+++ src/main/java/com/su/ac/th/project/grader/controller/AuthenticationController.java
@@ -1,9 +1,7 @@
 package com.su.ac.th.project.grader.controller;
 
 import com.su.ac.th.project.grader.model.request.UsersRegRequest;
-import com.su.ac.th.project.grader.model.request.UsersRequest;
 import com.su.ac.th.project.grader.service.AuthenticationService;
-import com.su.ac.th.project.grader.service.UsersService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

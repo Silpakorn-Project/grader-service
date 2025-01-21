6d5bd7f865a68eba4ae97554182532879c8b91de
sodtiruk <sodtiruk22@gmail.com>
2025-01-21 22:07:18 +0700

    refactor: remove comments

--- src/main/java/com/su/ac/th/project/grader/util/DtoEntityMapper.java
+++ src/main/java/com/su/ac/th/project/grader/util/DtoEntityMapper.java
@@ -9,10 +9,6 @@ import java.util.stream.Collectors;
 
 public class DtoEntityMapper {
 
-    /**
-     * -- GETTER --
-     *  Getter สำหรับ ModelMapper (กรณีต้องการปรับแต่ง Mapping ด้วยตัวเอง)
-     */
     @Getter
     private static final ModelMapper modelMapper = new ModelMapper();
 
@@ -21,32 +17,20 @@ public class DtoEntityMapper {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
 
-    /**
-     * Map จาก DTO ไปยัง Entity
-     */
     public static <DTO, Entity> Entity mapToEntity(DTO dto, Class<Entity> entityClass) {
         return modelMapper.map(dto, entityClass);
     }
 
-    /**
-     * Map จาก Entity ไปยัง DTO
-     */
     public static <Entity, DTO> DTO mapToDto(Entity entity, Class<DTO> dtoClass) {
         return modelMapper.map(entity, dtoClass);
     }
 
-    /**
-     * Map List ของ DTO ไปยัง List ของ Entity
-     */
     public static <DTO, Entity> List<Entity> mapListToEntity(List<DTO> dtoList, Class<Entity> entityClass) {
         return dtoList.stream()
                 .map(dto -> mapToEntity(dto, entityClass))
                 .collect(Collectors.toList());
     }
 
-    /**
-     * Map List ของ Entity ไปยัง List ของ DTO
-     */
     public static <Entity, DTO> List<DTO> mapListToDto(List<Entity> entityList, Class<DTO> dtoClass) {
         return entityList.stream()
                 .map(entity -> mapToDto(entity, dtoClass))

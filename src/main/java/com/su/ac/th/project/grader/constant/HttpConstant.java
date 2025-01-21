c5f891887afab60704730e2e5d303253e12a1d0d
sodtiruk <sodtiruk22@gmail.com>
2025-01-21 11:07:18 +0700

    feat: add constant common

---
+++ src/main/java/com/su/ac/th/project/grader/constant/HttpConstant.java
@@ -0,0 +1,23 @@
+package com.su.ac.th.project.grader.constant;
+
+public class HttpConstant {
+
+    public static class Status {
+        public static final int SUCCESS = 200;
+        public static final int BAD_REQUEST = 400;
+        public static final int UNAUTHORIZED = 401;
+        public static final int FORBIDDEN = 403;
+        public static final int NOT_FOUND = 404;
+        public static final int INTERNAL_SERVER_ERROR = 500;
+    }
+
+    public static class Message {
+        public static final String SUCCESS = "OK";
+        public static final String BAD_REQUEST = "Bad Request";
+        public static final String UNAUTHORIZED = "Unauthorized";
+        public static final String FORBIDDEN = "Forbidden";
+        public static final String NOT_FOUND = "Not Found";
+        public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    }
+
+}

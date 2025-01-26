de84bd53e87e1c17c2f42cbe85e7371cd3750b91
sodtiruk <sodtiruk22@gmail.com>
2025-01-26 07:35:27 +0700

    feat: add schema register and login in swagger

--- src/main/java/com/su/ac/th/project/grader/model/request/UsersLoginRequest.java
+++ src/main/java/com/su/ac/th/project/grader/model/request/UsersLoginRequest.java
@@ -1,11 +1,14 @@
 package com.su.ac.th.project.grader.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UsersLoginRequest {
 
+    @Schema(description = "Username", example = "admin")
     private String username;
+    @Schema(description = "Password", example = "1234")
     private String password;
 
 }
--- src/main/java/com/su/ac/th/project/grader/model/request/UsersRegRequest.java
+++ src/main/java/com/su/ac/th/project/grader/model/request/UsersRegRequest.java
@@ -1,12 +1,16 @@
 package com.su.ac.th.project.grader.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UsersRegRequest {
 
+    @Schema(description = "Username", example = "admin")
     private String username;
+    @Schema(description = "Password", example = "1234")
     private String password;
+    @Schema(description = "Email", example = "admin@gmail.com")
     private String email;
 
 }

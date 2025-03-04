f183c05e30677a841db0758ecb1480f9ea48b3c6
sodtiruk <sodtiruk22@gmail.com>
2025-03-04 21:54:43 +0700

    feat: add generate url swagger on command terminal

--- src/main/java/com/su/ac/th/project/grader/GraderApplication.java
+++ src/main/java/com/su/ac/th/project/grader/GraderApplication.java
@@ -1,13 +1,29 @@
 package com.su.ac.th.project.grader;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class GraderApplication {
+@Slf4j
+@RequiredArgsConstructor
+public class GraderApplication implements CommandLineRunner {
+
+    private final Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(GraderApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args){
+        String port = environment.getProperty("server.port", "8080");
+        String contextPath = environment.getProperty("server.servlet.context-path", "");
+        String swaggerUrl = "http://localhost:" + port + contextPath + "/swagger-ui.html";
+        log.info("Swagger UI is available at: {}", swaggerUrl);
     }
 
 }
